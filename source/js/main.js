cf495019eee550592e614594fe18eb3d76e2def8
cinis-ad-cinis <valentinakhramtsova@gmail.com>
2022-12-09 04:32:08 +0300

    add membership section

--- source/js/main.js
+++ source/js/main.js
@@ -22,44 +22,63 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 const vid = document.querySelector('.video');
+const tabButtons = document.querySelectorAll('.membership__button');
+const tabs = document.querySelectorAll('.membership__list');
 
 // Video
 
 const setupVideo = (video) => {
-  let link = video.querySelector('.video__link');
-  let button = video.querySelector('.video__button');
+  if (vid) {
+    let link = video.querySelector('.video__link');
+    let button = video.querySelector('.video__button');
 
-  video.addEventListener('click', () => {
-    let iframe = createIframe();
+    video.addEventListener('click', () => {
+      let iframe = createIframe();
 
-    link.remove();
-    button.remove();
-    video.appendChild(iframe);
+      link.remove();
+      button.remove();
+      video.appendChild(iframe);
+    });
+
+    link.removeAttribute('href');
+  }
+
+  const createIframe = () => {
+    let iframe = document.createElement('iframe');
+
+    iframe.setAttribute('allowfullscreen', '');
+    iframe.setAttribute('allow', 'autoplay');
+    iframe.setAttribute('src', generateURL());
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('width', '364px');
+    iframe.setAttribute('height', '228px');
+    iframe.classList.add('video__cover');
+
+    return iframe;
+  };
+
+  const generateURL = () => {
+    let id = vid.getAttribute('data-id');
+    let query = '?rel=0&showinfo=0&autoplay=1';
+
+    return 'https://www.youtube.com/embed/' + id + query;
+  };
+};
+
+// Tabs
+
+tabButtons.forEach((item) => {
+  item.addEventListener('click', (evt) => {
+    const id = evt.target.getAttribute('data-button');
+    const currentTab = document.querySelector(`[data-tab='${id}']`);
+
+    tabButtons.forEach((elem) => elem.classList.remove('membership__button--active'));
+    tabs.forEach((elem) => elem.classList.remove('membership__list--active'));
+
+    item.classList.add('membership__button--active');
+    currentTab.classList.add('membership__list--active');
   });
-
-  link.removeAttribute('href');
-};
-
-const createIframe = () => {
-  let iframe = document.createElement('iframe');
-
-  iframe.setAttribute('allowfullscreen', '');
-  iframe.setAttribute('allow', 'autoplay');
-  iframe.setAttribute('src', generateURL());
-  iframe.setAttribute('frameborder', '0');
-  iframe.setAttribute('width', '364px');
-  iframe.setAttribute('height', '228px');
-  iframe.classList.add('video__cover');
-
-  return iframe;
-};
-
-const generateURL = () => {
-  let id = vid.getAttribute('data-id');
-  let query = '?rel=0&showinfo=0&autoplay=1';
-
-  return 'https://www.youtube.com/embed/' + id + query;
-};
+});
 
 
 // ---------------------------------
